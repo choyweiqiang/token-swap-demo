206ba4f2739ddd8ab161a8da570ae7c838583e15
choyweiqiang <choyweiqiang@gmail.com>
2025-08-08 15:11:36 +0800

    feat: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ThemeProvider from './providers/ThemeProvider.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
